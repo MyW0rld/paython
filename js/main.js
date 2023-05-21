d4f5de551fee3ac48ffa219fda45eeafab803d82
Paython <119300945+MyW0rld@users.noreply.github.com>
2023-05-22 00:44:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
 const web = document.querySelector('#web')
+const host = document.querySelector('#host')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
 const webContent = document.querySelector('#web-content')
+const hostContent = document.querySelector('#host-content')
 
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
@@ -35,6 +37,25 @@ contact.addEventListener('click', () => {
     bottom: 0,
     left: 0,
     mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+host.addEventListener('click', () => {
+  const hostBox = new WinBox({
+    title: 'Web-Page Hosting',
+    width: '700px',
+    height: '400px',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    mount: hostContent,
     onfocus: function () {
       this.setBackground('#00aa00')
     },
